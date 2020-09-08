ea80628777c0bcc8513bb0058e0cb10b9cd7170d
gathoni-k <maryg.kahiga@gmail.com>
2020-09-08 20:45:51 +0300

    Add solution/ run standard js

--- server.js
+++ server.js
@@ -2,31 +2,50 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require('cors')
+app.use(cors({ optionSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' })
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
+app.get('/api/timestamp/:date_string?', (req, res) => {
+  try {
+    const dateString = req.params.date_string
+    if (!dateString) {
+      return res.json({
+        unix: new Date(Date.now()).getTime(),
+        utc: new Date(Date.now()).toUTCString()
+      })
+    }
+    const date = isNaN(dateString) ? new Date(dateString) : new Date(parseInt(dateString))
+    if (date == 'Invalid Date') throw Error()
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+  } catch (error) {
+    res.json({
+      error: 'Invalid date'
+    })
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
